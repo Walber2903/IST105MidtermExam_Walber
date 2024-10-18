e2e2b03805012bcf4a0233a36f39d018818141d3
Walber Menezes <walber.menezes@hotmail.com>
2024-10-17 20:40:54 -0700

    Initial commit

---
+++ public/js/firebase.js
@@ -0,0 +1,24 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVmC-HJD_YFP7bLS6a4csSFD00eiSI-y0",
+  authDomain: "ist105midtermexam-walber.firebaseapp.com",
+  projectId: "ist105midtermexam-walber",
+  storageBucket: "ist105midtermexam-walber.appspot.com",
+  messagingSenderId: "732367975639",
+  appId: "1:25609857269:web:a6d52ff12f323ff5bf3dbc"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
---
+++ public/js/main.js
@@ -0,0 +1,47 @@
+function authStateListener() {
+    // [START auth_state_listener]
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/v8/firebase.User
+            var uid = user.uid;
+            // ...
+            location.href = 'culturalconnections.html';
+        } else {
+            // User is signed out
+            // ...
+
+        }
+    });
+    // [END auth_state_listener]
+}
+
+window.addEventListener('load', function () {
+
+
+    //Listen for auth state changes
+    authStateListener();
+
+    document.getElementById('sign-in-2').addEventListener('click', function () {
+
+        let emailTxt = document.getElementById('email').value;
+        let passtxt = document.getElementById('password').value;
+
+        firebase.auth().signInWithEmailAndPassword(emailTxt, passtxt)
+            .then((userCredential) => {
+                // Signed in
+                let user = userCredential.user;
+                // ...
+                console.log('Logging sucessfully');
+                alert("Hello, I'm Walber. Welcome to my cultural connections page!");
+                location.href = 'culturalconnections.html';
+            })
+            .catch((error) => {
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                alert('Logging fail');
+                console.log('Logging fail', errorMessage);
+            });
+
+    });
+});
